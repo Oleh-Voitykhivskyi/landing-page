cc26a9399e83c963878b56e649236322e38fd6ea
Andrii447 <59208278+Andrii447@users.noreply.github.com>
2025-12-01 19:31:17 +0200

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,7 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/images/vasylchenko_photo.jpg"} alt="Andrii Vasylchenko" style={styles.photo} />
         <div style={styles.textContainer}>
           <h2 style={styles.name}>Andrii Vasylchenko</h2>
           <p style={styles.position}>Business analyst</p>
